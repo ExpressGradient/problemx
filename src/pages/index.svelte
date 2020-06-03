066eaa13afb0b1796d23dd50043d20ca06fc1491
Praneeth <johndoeknown@protonmail.com>
2020-06-03 15:18:20 +0530

    added search func

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,6 +1,5 @@
 <script>
     import Appbar from "./_components/Appbar.svelte";
-    import CustomSearchForm from "./_components/CustomSearchForm.svelte";
     import { firestore } from "./_firebase_utils";
     import { goto } from "@sveltech/routify";
 
@@ -9,6 +8,11 @@
     firestore.collection("problems").get().then((docs) => docs.forEach((doc) => {
         problems = [...problems, doc.data()];
     }));
+
+    let searchInput = "";
+    const submitSearch = () => {
+        problems = problems.filter((problem) => (searchInput === problem.title));
+    }
 </script>
 
 <svelte:head>
@@ -16,8 +20,18 @@
 </svelte:head>
 
 <Appbar />
-<CustomSearchForm />
-<div>
+
+<div class="search-form">
+    <form on:submit|preventDefault={submitSearch}>
+        <label>
+            Search Problem:
+            <input type="text" required bind:value={searchInput}>
+        </label>
+        <button type="submit">Search</button>
+    </form>
+</div>
+
+<div class="info">
     <p>You an org? Mail us!!!</p>
     <p>express.gradient@gmail.com</p>
 </div>
@@ -36,7 +50,7 @@
 </div>
 
 <style>
-    .problems-list {
+    .problems-list, .info, .search-form {
         font-size: 20px;
         text-align: center;
         color: #fff;
@@ -52,6 +66,31 @@
         padding: 10px;
     }
 
+    input {
+        margin-top: 15px;
+        background-color: lightgray;
+    }
+
+    input:focus {
+        outline: none;
+    }
+
+    button {
+        cursor: pointer;
+        width: 30%;
+        background-color: #7289DA;
+        color: #fff;
+        transition: ease-in 300ms;
+    }
+
+    button:hover {
+        font-size: 25px;
+    }
+
+    button:active {
+        font-size: 22px;
+    }
+
     @media screen and (min-width: 760px) {
         .problem-card {
             width: 50%;
@@ -65,6 +104,10 @@
 
         .problem-card:active {
             width: 52%;
+        }
+
+        input, button {
+            width: 17%;
         }
     }
 </style>
